f051e5483ede4b323458494341d880cc948e65f9
vithanhtu <71744958+vithanhtu@users.noreply.github.com>
2022-05-26 21:27:00 +0700

    Update Details.js

--- src/page/Details.js
+++ src/page/Details.js
@@ -1,6 +1,4 @@
 import React from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { useContext } from "react";
 import { ProductContext } from "../context";
 import { GrFormPrevious, GrFormNext } from "react-icons/gr";
